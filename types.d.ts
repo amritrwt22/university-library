021419e90e9e547de3c47a465f44c77f5e7a8a35
Amrit <amritrwt22@gmail.com>
2025-07-20 13:39:02 +0530

    feat: types interfaces for user, books etc and constant , sample data for app

--- types.d.ts
+++ types.d.ts
@@ -1,41 +1,51 @@
+/*this file is used to define TypeScript interfaces and types for a library management system, 
+including user, book, and authentication details.
+
+so we dont need to define types for every single object in the codebase.
+
+typescript interfaces and types are used to define the structure of objects.
+
+typescript interface is a way to define the shape of an object, including its properties and their types.
+*/
 interface Book {
-    id: string;
-    title: string;
-    author: string;
-    genre: string;
-    rating: number;
-    totalCopies: number;
-    availableCopies: number;
-    description: string;
-    coverColor: string;
-    coverUrl: string;
-    videoUrl: string;
-    summary: string;
-    createdAt: Date | null;
-  }
-  
-  interface AuthCredentials {
-    fullName: string;
-    email: string;
-    password: string;
-    universityId: number;
-    universityCard: string;
-  }
-  
-  interface BookParams {
-    title: string;
-    author: string;
-    genre: string;
-    rating: number;
-    coverUrl: string;
-    coverColor: string;
-    description: string;
-    totalCopies: number;
-    videoUrl: string;
-    summary: string;
-  }
-  
-  interface BorrowBookParams {
-    bookId: string;
-    userId: string;
-  }
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  total_copies: number;
+  available_copies: number;
+  description: string;
+  color: string;
+  cover: string;
+  videoUrl: string;
+  summary: string;
+  createdAt: Date | null;
+  isLoanedBook: boolean;
+}
+
+interface AuthCredentials {
+  fullName: string;
+  email: string;
+  password: string;
+  universityId: number;
+  universityCard: string;
+}
+
+interface BookParams {
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  coverUrl: string;
+  coverColor: string;
+  description: string;
+  totalCopies: number;
+  videoUrl: string;
+  summary: string;
+}
+
+interface BorrowBookParams {
+  bookId: string;
+  userId: string;
+}
