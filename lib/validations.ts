95b6868d2c5a229e1db4556c1a6eca0764de48f4
Amrit <amritrwt22@gmail.com>
2025-07-20 14:55:55 +0530

    feat: added authform and validation schemas

---
+++ lib/validations.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  fullName: z.string().min(3),
+  email: z.string().email(),
+  universityId: z.coerce.number(),
+  universityCard: z.string().nonempty("University Card is required"),
+  password: z.string().min(8),
+});
+
+export const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const bookSchema = z.object({
+  title: z.string().trim().min(2).max(100),
+  description: z.string().trim().min(10).max(1000),
+  author: z.string().trim().min(2).max(100),
+  genre: z.string().trim().min(2).max(50),
+  rating: z.coerce.number().min(1).max(5),
+  totalCopies: z.coerce.number().int().positive().lte(10000),
+  coverUrl: z.string().nonempty(),
+  coverColor: z
+    .string()
+    .trim()
+    .regex(/^#[0-9A-F]{6}$/i),
+  videoUrl: z.string().nonempty(),
+  summary: z.string().trim().min(10),
+});
+
+// this file is used to define validation schemas for user sign-up, sign-in, and book creation forms using zod.
+// z is a TypeScript-first schema declaration and validation library.
+// it provides a way to define schemas for objects and validate them against the defined schemas.
+// these schemas are used to validate user input in forms, ensuring that the data is in the correct format and meets the specified requirements.
