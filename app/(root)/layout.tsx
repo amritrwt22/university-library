31322a6ed1c77cd1afecba58f2452bb3463734d6
Amrit <amritrwt22@gmail.com>
2025-07-31 18:15:53 +0530

    root page and layout , added session to header comp in root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,16 @@
 import { ReactNode } from "react";
-import Header from "@/components/Header";
-// import { auth } from "@/auth";
-// import { redirect } from "next/navigation";
+import { Header } from "@/components/Header";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 // import { after } from "next/server";
 // import { db } from "@/database/drizzle";
 // import { users } from "@/database/schema";
 // import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-  // const session = await auth();
+  const session = await auth();
 
-  // if (!session) redirect("/sign-in");
+  if (!session) redirect("/sign-in");
 
   // after(async () => {
   //   if (!session?.user?.id) return;
@@ -33,11 +33,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
   return (
     <main className="root-container ">
       <div className="mx-auto max-w-7xl">
-        {/* <Header session={session} /> */}
-        <Header/>
-        
-        
-
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
---
+++ app/(root)/page.tsx
@@ -0,0 +1,31 @@
+import { BookOverview } from "@/components/BookOverview";
+import { BookList } from "@/components/BookList";
+import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+
+const Home = async () => {
+  //   const session = await auth();
+
+  //   const latestBooks = (await db
+  //     .select()
+  //     .from(books)
+  //     .limit(10)
+  //     .orderBy(desc(books.createdAt))) as Book[];
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+
+  return (
+    <>
+      {/* sampleBooks is an array of objects, each obj contains details of a book , present in constants*/}
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
+
+export default Home;
